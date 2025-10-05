4b968d762e5a77f41b6dd7633f79c81dee9e396c
kiernan-dev <kiernan.devsignr@gmail.com>
2025-10-04 20:32:17 -0500

    update astro config w/server rules

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,23 @@ import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
 
 export default defineConfig({
+  base: '/memorium',
   output: 'hybrid',
   adapter: node({
     mode: 'standalone'
   }),
+  server: {
+    host: '0.0.0.0',
+    port: 4321
+  },
+  vite: {
+    server: {
+      allowedHosts: [
+        'kiernan.studio',
+        '.kiernan.studio',
+        '.traefik.me'
+      ]
+    }
+  },
   integrations: [tailwind()],
 });
